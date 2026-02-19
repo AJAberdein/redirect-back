bbf3d430b7bdf373815d31573b7f384a712d104d
AJ Aberdein <ashleylouis.aberdein@vcontractor.co.za>
2026-02-19 08:24:56 +0000

    m

--- src/views/ChildC.vue
+++ src/views/ChildC.vue
@@ -4,6 +4,17 @@
   </div>
 </template>
 
+<script setup lang="ts">
+import { onBeforeRouteLeave } from "vue-router";
+
+onBeforeRouteLeave((to, from, next) => {
+  const confirmLeave = window.confirm("Are you sure you want to go back?");
+
+  if (confirmLeave) next();
+  else next(false);
+});
+</script>
+
 <style>
 @media (min-width: 1024px) {
   .about {
